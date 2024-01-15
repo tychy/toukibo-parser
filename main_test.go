ea570ee38d7416cdb7a4f923585daeb11106a094
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2024-01-16 00:01:41 +0900

    add test for 10

--- main_test.go
+++ main_test.go
@@ -15,7 +15,7 @@ type TestData struct {
 }
 
 func TestToukiboParser(t *testing.T) {
-	testCount := 2
+	testCount := 10
 
 	for i := 1; i <= testCount; i++ {
 		t.Run(fmt.Sprintf("test%d", i), func(t *testing.T) {
