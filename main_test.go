c8785935d90f722af59dc5759eb0b82bb15f846e
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2024-05-19 13:42:16 +0900

    pass 450

--- main_test.go
+++ main_test.go
@@ -22,7 +22,7 @@ type TestData struct {
 }
 
 func TestToukiboParser(t *testing.T) {
-	testCount := 400
+	testCount := 450
 
 	for i := 1; i <= testCount; i++ {
 		t.Run(fmt.Sprintf("test%d", i), func(t *testing.T) {
