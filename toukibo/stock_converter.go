b02ea690d1515dabcc485a65dc2efe155b6bc094
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2024-09-18 16:09:35 +0900

    define struct stock

--- toukibo/stock_converter.go
+++ toukibo/stock_converter.go
@@ -1,20 +1,19 @@
 package toukibo
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 )
 
-func StockToNumber(stock string) int {
-	// 発行済株式の総数５万株 → 50000
-	stock = strings.Replace(stock, "発行済株式の総数", "", -1)
-	if strings.HasPrefix(stock, "普通株式") { // sample1082用のハック
-		stock = strings.Replace(stock, "普通株式", "", -1)
-	}
-	stock = ZenkakuToHankaku(stock)
-
+func GetStockNumber(s string) (int, string) {
 	sums := 0
 	cur := 0
-	for _, v := range stock {
+	foundKabu := false
+	for idx, v := range s {
+		if foundKabu {
+			return sums, s[idx:]
+		}
 		if v >= '0' && v <= '9' {
 			cur = cur*10 + int(v-'0')
 			continue
@@ -32,10 +31,86 @@ func StockToNumber(stock string) int {
 			cur = 0
 		case '株':
 			sums += cur
-			return sums
+			cur = 0 // 意味はない
+			foundKabu = true
 			// 発行済株式の総数４万８２４９株各種の株式の数普通株式　　　３万２４９株Ａ種優先株式　１万株Ｂ種優先株式　８０００株
 			//　のようなパターンでは最初の株でReturnさせる
+			continue
 		}
 	}
-	return sums
+	return sums, ""
+}
+
+type PreferredStock struct {
+	Name string
+	Num  int
+}
+
+type Stock struct {
+	Total     int
+	Preferred []PreferredStock
+}
+
+func (s Stock) Sum() int {
+	sum := 0
+	for _, p := range s.Preferred {
+		sum += p.Num
+	}
+	return sum
+}
+
+func StockToNumber(stock string) int {
+	stock = ZenkakuToHankaku(stock)
+	stock = trimAllSpace(stock)
+	res := Stock{}
+
+	for {
+		if stock == "" {
+			break
+		}
+
+		if strings.HasPrefix(stock, "発行済株式の総数") {
+			stock = strings.Replace(stock, "発行済株式の総数", "", -1)
+			if strings.HasPrefix(stock, "普通株式") {
+				continue // sample1082用のハック
+			}
+
+			sums, s := GetStockNumber(stock)
+			stock = s
+			res.Total = sums
+			continue
+		}
+
+		if strings.HasPrefix(stock, "各種の株式の数") {
+			stock = strings.Replace(stock, "各種の株式の数", "", -1)
+			continue
+		}
+
+		if strings.HasPrefix(stock, "普通株式") {
+			stock = strings.Replace(stock, "普通株式", "", -1)
+			normal, s := GetStockNumber(stock)
+			stock = s
+			res.Preferred = append(res.Preferred, PreferredStock{Name: "普通株式", Num: normal})
+			continue
+		}
+
+		// *優先株式　で始まる場合
+		pattern := fmt.Sprintf("([%s]+優先株式)", ZenkakuNoNumberStringPattern)
+		regex := regexp.MustCompile(pattern)
+		matches := regex.FindStringSubmatch(stock)
+		if len(matches) > 0 {
+			stock = strings.Replace(stock, matches[1], "", -1)
+			num, s := GetStockNumber(stock)
+			stock = s
+			res.Preferred = append(res.Preferred, PreferredStock{Name: matches[1], Num: num})
+			continue
+		}
+
+		break
+	}
+	if res.Total == 0 {
+		res.Total = res.Sum()
+	}
+
+	return res.Total
 }
